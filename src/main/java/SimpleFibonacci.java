0ff4f9058a32722201eade7625f26e761d56478c
BhavyaThota102 <bhavyathota2@gmail.com>
2024-09-02 10:24:34 +0530

    updated the data

--- src/main/java/SimpleFibonacci.java
+++ src/main/java/SimpleFibonacci.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class SimpleFibonacci {
+
+    public static void main(String[] args) {
+        int n = 10; // Number of terms in the Fibonacci series
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: " + a + " " + b);
+
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+    }
 }
--- src1/main/java/demo.java
+++ src1/main/java/demo.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class demo {
+    public static void main(String []args){
+        System.out.println("here is the new src file");
+    }
 }
